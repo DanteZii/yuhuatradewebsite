b2c3a492633a6016be980a2ed58551110249a1c4
Dante <guge862055200@gmail.com>
2018-08-29 21:49:02 +0200

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,18 @@ module.exports = {
             title: '裕华工贸',
             description: '我们是一家集全球采购、加工（可OEM）及销售于一体的线上及线下加工与贸易公司'
         }
-    },
+    }, 
+    head: [
+        ['link', { rel: 'icon', href: `/hero.png` }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#387BBC' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: `/hero.png` }],
+        ['link', { rel: 'mask-icon', href: '/hero.svg', color: '#387BBC' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/hero.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
     themeConfig: {
         sidebar: [
             {
